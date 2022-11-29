6a305c5906aa50875d9e7e4b6d94ab0e79e0a022
Rosano Coutinho <pub@xeari.com>
2022-11-29 09:47:00 +0100

    Purge dependencies

--- package-postinstall.js
+++ package-postinstall.js
@@ -20,6 +20,5 @@
 		'launchlet',
 		'ROCORootLink',
 		'webnative',
-		'choices.js',
 	], require('path').join(__dirname, 'node_modules'), require('path').join(__dirname, 'os-app/_shared/__external'));
 })();
