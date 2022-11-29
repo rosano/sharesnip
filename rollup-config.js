46ddffb3fb6ced7fc30e3ab1328dbdab9a91fe0e
Rosano Coutinho <pub@xeari.com>
2022-11-29 09:39:19 +0100

    Purge JBX_PLAY_PROXY_URL_TEMPLATE

--- rollup-config.js
+++ rollup-config.js
@@ -1,6 +1,5 @@
 export default require('OLSKRollupScaffold').OLSKRollupScaffoldScanStart(__dirname, {
 	OLSKRollupPluginSwapTokens: Object.assign(require('OLSKUIAssets').OLSKUIAssetsSwapTokens(), {
-		JBX_PLAY_PROXY_URL_TEMPLATE_SWAP_TOKEN: process.env.JBX_PLAY_PROXY_URL_TEMPLATE,
 		OLSK_APROPOS_FEEDBACK_EMAIL_SWAP_TOKEN: Buffer.from(`mailto:${ process.env.OLSK_APROPOS_FEEDBACK_EMAIL }`).toString('base64'),
 
  		OLSK_FUND_API_URL_SWAP_TOKEN: process.env.OLSK_FUND_API_URL,
