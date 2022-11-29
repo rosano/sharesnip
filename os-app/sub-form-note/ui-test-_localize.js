65071877507438e34fe1334c9821aae85f456cb6
Rosano Coutinho <pub@xeari.com>
2022-11-29 17:16:38 +0100

    Fix typos

--- os-app/sub-form-note/ui-test-_localize.js
+++ os-app/sub-form-note/ui-test-_localize.js
@@ -6,7 +6,7 @@ kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 		return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
 	};
 
-	describe('SNPShare_Localize-' + OLSKRoutingLanguage, function () {
+	describe('SNPCodeFormNote_Localize-' + OLSKRoutingLanguage, function () {
 
 		before(function() {
 			return browser.OLSKVisit(kDefaultRoute, {
--- os-app/sub-make/ui-test-_localize.js
+++ os-app/sub-make/ui-test-_localize.js
@@ -6,7 +6,7 @@ kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 		return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
 	};
 
-	describe('SNPShare_Localize-' + OLSKRoutingLanguage, function () {
+	describe('SNPCodeMake_Localize-' + OLSKRoutingLanguage, function () {
 
 		before(function() {
 			return browser.OLSKVisit(kDefaultRoute, {
